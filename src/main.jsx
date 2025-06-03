469480f32763133fe6e38143cfc778c4af80cc87
bongssss <etimubongabasi@gmail.com>
2025-06-03 22:14:38 +0100

    oneline change

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { setAuthToken } from './lib/api'  // ✅ Import this
+import { setAuthToken } from './lib/api'  
 
 // 🔧 TEMP: Hardcode token for dev testing
 localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1Ym9uZ0B1Ym9uZy5jb20iLCJleHAiOjE3NDg5ODY3NDJ9.Hpx5_SjQ7GmR-RMRHIGvZFY5SdUMID_jSF8kG_bcvj4');

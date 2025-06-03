90797b1c733e5aeb472acf691beef238606a41a5
bongssss <etimubongabasi@gmail.com>
2025-06-03 22:17:01 +0100

    one liner

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { setAuthToken } from './lib/api'  
 
-// 🔧 TEMP: Hardcode token for dev testing
+// 🔧 temporarily Hardcode token for dev testing
 localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1Ym9uZ0B1Ym9uZy5jb20iLCJleHAiOjE3NDg5ODY3NDJ9.Hpx5_SjQ7GmR-RMRHIGvZFY5SdUMID_jSF8kG_bcvj4');
 
 // Ensure axios attaches token to all future requests

f2d433b6350a442ebb1fa823859bc3f2177cb9ce
bongssss <etimubongabasi@gmail.com>
2025-06-03 22:16:16 +0100

    one liner

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { setAuthToken } from './lib/api'
 // 🔧 TEMP: Hardcode token for dev testing
 localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1Ym9uZ0B1Ym9uZy5jb20iLCJleHAiOjE3NDg5ODY3NDJ9.Hpx5_SjQ7GmR-RMRHIGvZFY5SdUMID_jSF8kG_bcvj4');
 
-// ✅ Ensure axios attaches token to all future requests
+// Ensure axios attaches token to all future requests
 const token = localStorage.getItem('token');
 if (token) {
   setAuthToken(token);
